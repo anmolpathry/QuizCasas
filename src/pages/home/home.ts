746e4a85ce1955d36d8ab4360056170bff5fc9cb
alumno Secundaria <al3201@cedi.local>
2019-02-08 09:01:08 -0600

    terminado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
+  casas = [];
+
   constructor(public navCtrl: NavController, public http:HttpClient) {
-    this.http.get('/v1/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)')
+    this.http.get('/v1/pin-data?url=/s-renta-inmuebles/guadalajara-y-zona-metro/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)')
     .subscribe(data => {
-      console.log(JSON.stringify(data));
+      //console.log(JSON.stringify(data));
+      if(data.hasOwnProperty('ads')){
+        this.casas = data['ads'];
+      }
     }, error => {
       console.log(JSON.stringify(error));
     });
   }
 
 }
+
+//Todos las  se encuentran dentro del diccionario ads
+
+//Precio => se encuentra en price
+//Precio= casa.price.priceType 
+//Moneda = casa.price.currency 
+//Cantidad= casa.price.formattedAmount
+
+// Ubicación => se encuentra en geo 
+// Municipio= casa.geo.name
+// Colonia= casa.geo.displayName
+
+//Imagen => se encuentra en el arreglo pictures
+// Imagen = casa.pictures[0].url
